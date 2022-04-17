d1885e39983f4c75272b61d5fc5ff8954fb0213d
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-04-17 19:59:50 -0300

    adicionado criacao de tópico e expansao forum

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-const resumido = document.getElementsByClassName('texto__resumido')[0]
-const completo = document.getElementsByClassName('texto__completo')[0]
+const resumido = document.getElementsByClassName('texto__resumido')[0];
+const completo = document.getElementsByClassName('texto__completo')[0];
+const discussao = document.getElementsByClassName('assunto__conteudo')[0];
+const discussaoCompleta = document.getElementsByClassName('assunto__conteudo__completo')[0];
+const discussoes = document.getElementsByClassName('discussoes__criar__topico')[0];
+const criarTopico = document.getElementsByClassName('criar__topico__botao')[0];
+const discussaoForm = document.getElementsByClassName('discussoes__form')[0];
+const conteudoNovoTopico = document.getElementsByClassName('conteudo__novo__topico')[0];
 
 document.addEventListener('click', e => {
-  const el = e.target;
+  const el = e.target.className;
 
-  if (el.matches('.expandir')) {
+  if (el.includes('expandir')) {
     resumido.style.display = 'none';
-    completo.style.display = 'block'
+    completo.style.display = 'block';
   }
 
-  if (el.matches('.resumir')) {
+  if (el.includes('resumir')) {
     completo.style.display = 'none';
-    resumido.style.display = 'block'
+    resumido.style.display = 'block';
+  }
+
+  if (el.includes('assunto__conteudo') || el.includes('caixa__conteudo')){
+    discussao.style.display = 'none'
+    discussaoCompleta.style.display = 'block'
+  }
+
+  if (el.includes('caixa__conteudo') || el.includes('discussao__lista') || el.includes('discussao__card') || el.includes('discussao__autor')) {
+    discussaoCompleta.style.display = 'none'
+    discussao.style.display = 'block'
+  }
+
+  if (el.includes('criar__topico')) {
+    discussaoForm.style.display = 'flex';
+    discussoes.style.display = 'none';
+    criarTopico.style.display = 'none';
+  }
+
+  if (el.includes('enviar__topico')) {
+    discussaoForm.style.display = 'none';
+    conteudoNovoTopico.style.display = 'block';
   }
 })
-
-document.get
